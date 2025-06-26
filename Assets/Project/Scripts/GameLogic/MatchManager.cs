869ff97123680fdaec4e366acd9f93f52d01219c
Çağatay Bayram GÖNEN <cagataybayramgonen@gmail.com>
2025-06-26 06:20:41 +0300

    feat: Tested the logic of matching

--- Assets/Project/Scripts/GameLogic/MatchManager.cs
+++ Assets/Project/Scripts/GameLogic/MatchManager.cs
@@ -9,31 +9,37 @@ public class MatchManager : MonoBehaviour
 
         if(isItemCorrect && isIslandCorrect)
         {
-            OnPerfectMatch(npc);
+            OnPerfectMatch(npc,item,island);
         }
         else if(isItemCorrect || isIslandCorrect)
         {
-            OnPartialMatch(npc);
+            OnPartialMatch(npc, item, island);
         }
         else
         {
-            OnMismatch(npc);
+            OnMismatch(npc, item, island);
         }
     }
-    void OnPerfectMatch(NPCData npc)
+    void OnPerfectMatch(NPCData npc, ItemData item, IslandData island)
     {
         Debug.Log($"{npc.npcName} found peace. Perfect match.");
+        Debug.Log($"NPC itemi = {npc.correctItemID}, olan item = {item.itemID} ");
+        Debug.Log($"NPC isldan = {npc.correctIslandID}, olan island = {island.islandID} ");
         // TODO: VFX, sound, fade-out, score++
     }
-    void OnPartialMatch(NPCData npc)
+    void OnPartialMatch(NPCData npc, ItemData item, IslandData island)
     {
         Debug.Log($"{npc.npcName} moved on, but not fully at peace.");
+        Debug.Log($"NPC itemi = {npc.correctItemID}, olan item = {item.itemID} ");
+        Debug.Log($"NPC isldan = {npc.correctIslandID}, olan island = {island.islandID} ");
         // TODO: sadder music, slower fade
     }
 
-    void OnMismatch(NPCData npc)
+    void OnMismatch(NPCData npc, ItemData item, IslandData island)
     {
         Debug.Log($"{npc.npcName} is still restless. Try again.");
+        Debug.Log($"NPC itemi = {npc.correctItemID}, olan item = {item.itemID} ");
+        Debug.Log($"NPC isldan = {npc.correctIslandID}, olan island = {island.islandID} ");
         // TODO: red glow, error sound, NPC stays / respawns later
     }
 }
