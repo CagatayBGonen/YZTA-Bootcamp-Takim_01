bb6ea531eea70e63fe5e578ff9be3ebb57c05150
Çağatay Bayram GÖNEN <cagataybayramgonen@gmail.com>
2025-07-10 18:19:10 +0300

    feat: Added Trait Enum & TraitSet Scripts

--- Assets/Project/Scripts/DialogueSystem/TraitSet.cs
+++ Assets/Project/Scripts/DialogueSystem/TraitSet.cs
@@ -1,16 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Dialogue/TraitSet")]
 public class TraitSet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Dictionary<TraitType, int> traits = new();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void SetTrait(TraitType trait, int level) => traits[trait] = level;
+    public int GetTraitLevel(TraitType trait) => traits.TryGetValue(trait, out var val) ? val : 0;
 }
--- Assets/Project/Scripts/DialogueSystem/TraitType.cs
+++ Assets/Project/Scripts/DialogueSystem/TraitType.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
 
-public class TraitType : MonoBehaviour
+public enum TraitType
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Empath,
+    Logic,
+    Skepticism
 }
+    
+
