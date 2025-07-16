e7f6667a7d2d07b5db5e640851bb3f22f81b791b
Çağatay Bayram GÖNEN <cagataybayramgonen@gmail.com>
2025-07-16 06:02:54 +0300

    feat: Added DialogueManager

--- Assets/Project/Scripts/DialogueSystem/DialogueState.cs
+++ Assets/Project/Scripts/DialogueSystem/DialogueState.cs
@@ -4,6 +4,7 @@ public enum DialogueState
 {
     // Represent the dialogue states for that NPC
     Undiscovered, // not yet seen
+    Available,    // Can be said or choose
     Success,      // Successfully discovered 
     Failed,       // Failed the dailogue   
     Locked        // Not having enough trait level
